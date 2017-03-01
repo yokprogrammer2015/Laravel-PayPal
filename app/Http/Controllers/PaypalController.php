31de53a699167013194d6864bc6bfc5571caa9e4
Puchong <puchong@songserm.co.th>
2017-03-01 13:42:51 +0700

    Laravel PayPal

---
+++ app/Http/Controllers/PaypalController.php
@@ -0,0 +1,146 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Laracasts\Flash\Flash;
+use Session;
+use Illuminate\Http\Request;
+use PayPal\Rest\ApiContext;
+use PayPal\Auth\OAuthTokenCredential;
+use PayPal\Api\Amount;
+use PayPal\Api\Details;
+use PayPal\Api\Item;
+use PayPal\Api\ItemList;
+use PayPal\Api\Payer;
+use PayPal\Api\Payment;
+use PayPal\Api\RedirectUrls;
+use PayPal\Api\ExecutePayment;
+use PayPal\Exception\PayPalConnectionException;
+use PayPal\Api\PaymentExecution;
+use PayPal\Api\Transaction;
+
+class PaypalController extends Controller
+{
+    private $_api_context;
+
+    public function __construct()
+    {
+        // setup PayPal api context
+        $paypal_conf = config('paypal');
+        $this->_api_context = new ApiContext(new OAuthTokenCredential($paypal_conf['client_id'], $paypal_conf['secret']));
+        $this->_api_context->setConfig($paypal_conf['settings']);
+
+    }
+
+    public function postPayment()
+    {
+
+        $payer = new Payer();
+        $payer->setPaymentMethod('paypal');
+
+        $item_1 = new Item();
+        $item_1->setName('Item 1')// item name
+        ->setCurrency('USD')
+            ->setQuantity(2)
+            ->setPrice('15'); // unit price
+
+        // add item to list
+        $item_list = new ItemList();
+        $item_list->setItems(array($item_1));
+
+        $amount = new Amount();
+        $amount->setCurrency('USD')
+            ->setTotal(30);
+
+        $transaction = new Transaction();
+        $transaction->setAmount($amount)
+            ->setItemList($item_list)
+            ->setDescription('Your transaction description');
+
+        $redirect_urls = new RedirectUrls();
+        $redirect_urls->setReturnUrl(url('payment/status'))
+            ->setCancelUrl(url('payment/status'));
+
+        $payment = new Payment();
+        $payment->setIntent('Agent')
+            ->setPayer($payer)
+            ->setRedirectUrls($redirect_urls)
+            ->setTransactions(array($transaction));
+
+        try {
+            $payment->create($this->_api_context);
+        } catch (PayPalConnectionException $ex) {
+            if (\config('app.debug')) {
+                echo "Exception: " . $ex->getMessage() . PHP_EOL;
+                $err_data = json_decode($ex->getData(), true);
+                exit;
+            } else {
+                Flash::error('Something went wrong, Sorry for inconvenience');
+                return redirect('/');
+            }
+        }
+
+        foreach ($payment->getLinks() as $link) {
+            if ($link->getRel() == 'approval_url') {
+                $redirect_url = $link->getHref();
+                break;
+            }
+        }
+
+        // add payment ID to session
+        Session::put('paypal_payment_id', $payment->getId());
+
+        if (isset($redirect_url)) {
+            // redirect to paypal
+            return redirect($redirect_url);
+        }
+        Flash::error('Unknown error occurred');
+        return redirect('/');
+    }
+
+    public function getPaymentStatus(Request $request)
+    {
+        // Get the payment ID before session clear
+        $payment_id = Session::get('paypal_payment_id');
+
+        // clear the session payment ID
+        Session::forget('paypal_payment_id');
+
+        if (empty($request->input('PayerID')) || empty($request->input('token'))) {
+            Flash::error('Payment Failed');
+            return redirect('/');
+        }
+
+        $payment = Payment::get($payment_id, $this->_api_context);
+
+        // PaymentExecution object includes information necessary
+        // to execute a PayPal account payment.
+        // The payer_id is added to the request query parameters
+        // when the user is redirected from paypal back to your site
+        $execution = new PaymentExecution();
+        $execution->setPayerId($request->input('PayerID'));
+
+        //Execute the payment
+        $result = $payment->execute($execution, $this->_api_context);
+
+        /*
+        * Get the ID with : $result->id
+        * Get the State with $result->state
+        * Get the Payer State with $result->payer->payment_method
+        * Get The Shipping Address and More Detail like below :- $result->payer->payer_info->shipping_address
+        * Get More detail about shipping address like city country name
+        */
+
+        echo '<pre>';
+        print_r($result);
+        echo '</pre>';
+        exit; // DEBUG RESULT.
+
+        if ($result->getState() == 'approved') { // payment made
+            Flash::success('Payment Successful');
+            return redirect('home');
+        }
+        Flash::error('Payment Failed');
+        return redirect('/');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// You can use "get" or "post" method below for payment..
+Route::get('payment', 'PaypalController@postPayment');
+// This must be get method.
+Route::get('payment/status', 'PaypalController@getPaymentStatus');
